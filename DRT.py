060397e57cbc385ec2b01d5e5ec7f5dd8adb2e6d
zaghlol <zaghlollight@gmail.com>
2019-05-27 14:10:47 +0200

    test changes

--- DRT.py
+++ DRT.py
@@ -10,6 +10,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
+
+
 dataset = pd.read_csv('Position_Salaries.csv')
 x=dataset.iloc[:,1:2].values
 y=dataset.iloc[:,2].values
